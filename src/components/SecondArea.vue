dcebd7627381071f34005bee154ea07a98f48cfc
kingryan9996 <kingryan9996@gmail.com>
2023-04-10 09:53:06 +0900

    last1222

--- src/components/SecondArea.vue
+++ src/components/SecondArea.vue
@@ -86,7 +86,7 @@
               
               // padding-top: ${workList[0].clientHeight / 5}px;
               // margin-bottom: ${workList[0].clientHeight / 2}px`;
-  
+                // ??
               chapterBorder.style = `
               position: absolute;
               border-top: 1px solid #000;
